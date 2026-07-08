fc9f4b4794f2235104862df39140f7399c48f995
mihajlvmisa1-lang <mihajlvmisa1@gmail.com>
2026-07-09 00:10:02 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import time
 import os
 from playwright.sync_api import sync_playwright
 
-EPG_URL = "https://example.com/epg.xml.gz"
+EPG_URL = "https://iptvx.one/EPG_NOARCH"
 
 def get_m3u8_with_click(url):
     m3u8_link = None
